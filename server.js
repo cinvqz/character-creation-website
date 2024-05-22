a3de16a90066272b61da2e1804748a1be2432105
Soul <joshgingold336@gmail.com>
2024-05-21 20:07:53 -0500

    initial commit (added necessary files with proper structuring)

---
+++ server.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const helpers = require("./utils/helper");
+
+const sequelize = require("./config/connection");
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Create the Handlebars.js engine object with custom helper functions
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const userRoutes = require("./routes/api/userRoutes");
+const homeRoutes = require("./controllers/homeRoutes");
+
+// Using routes
+app.use("/api/users", userRoutes);
+app.use("/", homeRoutes);
+
+// Inform Express.js which template engine we're using
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening on localhost:" + PORT));
+});
