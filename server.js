b8d0d680ab8b1b7054612671a20521d0d3346276
Soul <joshgingold336@gmail.com>
2024-06-03 21:36:39 -0500

    nav bar fix

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-const helpers = require("./utils/helper");
 const sequelize = require("./config/connection");
 const routes = require("./routes/index");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create();
 
 const sess = {
   secret: "Super secret secret",
